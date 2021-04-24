59f134b1a062f8f124a8e535b2556becde00b8b9
ralph <ralph.schneller@gmail.com>
2021-04-24 10:45:47 +0200

    Returning JSON

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express()
 const port = process.env.PORT || 1337
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  
+  res.contentType("application/json")
+  
+  let result = {
+    monthlyBase: 1488.0,
+    monthlyBonus: 42,
+    monthlyTotal: 1530
+  }
+  res.send(result)
 })
 
 app.listen(port, () => {
